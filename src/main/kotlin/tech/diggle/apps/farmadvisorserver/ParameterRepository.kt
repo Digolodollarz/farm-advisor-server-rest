54a80a908c7770181f8bbfc915b3260aefb4b5d1
Eddie <eddie@diggle.tech>
2019-04-26 06:31:40 +0200

    Eh some CORS stuff whatever

--- src/main/kotlin/tech/diggle/apps/farmadvisorserver/ParameterRepository.kt
+++ src/main/kotlin/tech/diggle/apps/farmadvisorserver/ParameterRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource
 @RepositoryRestResource(collectionResourceRel = "parameters", path = "parameters")
 interface ParameterRepository : PagingAndSortingRepository<Parameter, Long> {
     fun findTopByOrderByIdDesc(): Parameter?
+    fun findTopByOrderById(): Parameter?
 }
